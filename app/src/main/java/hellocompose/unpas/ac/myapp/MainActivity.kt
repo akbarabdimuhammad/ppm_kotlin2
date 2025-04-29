e1ecdfb6c5d375b9d9f6c32e80fe2bcc24c3a9f6
akbarabdimuhammad <akbarabdimuhammad@gmail.com>
2025-04-30 00:37:50 +0700

    latihan1 PPM3_kotlin

--- app/src/main/java/hellocompose/unpas/ac/myapp/MainActivity.kt
+++ app/src/main/java/hellocompose/unpas/ac/myapp/MainActivity.kt
@@ -1,29 +1,34 @@
 package hellocompose.unpas.ac.myapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.activity.compose.LocalContext
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import hellocompose.unpas.ac.myapp.ui.theme.MyAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import hellocompose.unpas.ac.myapp.ui.theme.HelloComposeTheme
+import hellocompose.unpas.ac.myapp.ui.theme.Pink40
+import hellocompose.unpas.ac.myapp.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyAppTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            HelloComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    FormLogin(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +36,86 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello world!",
-        modifier = modifier
-    )
+fun FormLogin(modifier: Modifier = Modifier) {
+    val context = LocalContext.current  // Mengambil context aplikasi
+    val username = remember { mutableStateOf(TextFieldValue("")) }
+    val password = remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
+        Text(text = "Username", modifier = Modifier.padding(4.dp).fillMaxWidth())
+        TextField(
+            value = username.value,
+            onValueChange = { username.value = it },
+            modifier = Modifier.padding(4.dp).fillMaxWidth()
+        )
+
+        Text(text = "Password", modifier = Modifier.padding(4.dp).fillMaxWidth())
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.padding(4.dp).fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            // Tombol Login
+            Button(
+                onClick = {
+                    if (username.value.text == "admin" && password.value.text == "admin") {
+                        Toast.makeText(context, "Login Sukses", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(context, "Login Gagal", Toast.LENGTH_LONG).show()
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Purple80,
+                    contentColor = Pink40
+                ),
+                modifier = Modifier.weight(5f).padding(end = 4.dp)
+            ) {
+                Text(
+                    text = "Login",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 18.sp
+                    ),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+
+            // Tombol Reset
+            Button(
+                modifier = Modifier.weight(5f).padding(start = 4.dp),
+                onClick = {
+                    // Reset username dan password
+                    username.value = TextFieldValue("")
+                    password.value = TextFieldValue("")
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Pink40,
+                    contentColor = Purple80
+                )
+            ) {
+                Text(
+                    text = "Reset",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 18.sp
+                    ),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyAppTheme {
-        Greeting("Android")
+    HelloComposeTheme {
+        FormLogin()
     }
 }
--- app/src/main/java/hellocompose/unpas/ac/myapp/ui/theme/Theme.kt
+++ app/src/main/java/hellocompose/unpas/ac/myapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package hellocompose.unpas.ac.myapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -21,7 +21,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -34,24 +33,23 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MyAppTheme(
+fun HelloComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        typography = Typography, // pastikan file Typography.kt ada
+        content = content
     )
 }
