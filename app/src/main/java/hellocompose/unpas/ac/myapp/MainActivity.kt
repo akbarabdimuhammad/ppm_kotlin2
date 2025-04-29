6191b23e5bb6fc1e659ca5600dc31d8e428da3cd
akbarabdimuhammad <akbarabdimuhammad@gmail.com>
2025-04-30 00:40:24 +0700

    latihan2 PPM3_kotlin

--- app/src/main/java/hellocompose/unpas/ac/myapp/MainActivity.kt
+++ app/src/main/java/hellocompose/unpas/ac/myapp/MainActivity.kt
@@ -1,112 +1,107 @@
-package hellocompose.unpas.ac.myapp
+package com.example.registrationapp
 
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.compose.LocalContext
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import hellocompose.unpas.ac.myapp.ui.theme.HelloComposeTheme
-import hellocompose.unpas.ac.myapp.ui.theme.Pink40
-import hellocompose.unpas.ac.myapp.ui.theme.Purple80
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormLogin(modifier = Modifier.padding(innerPadding))
-                }
-            }
+            RegistrationForm()
         }
     }
 }
 
 @Composable
-fun FormLogin(modifier: Modifier = Modifier) {
-    val context = LocalContext.current  // Mengambil context aplikasi
-    val username = remember { mutableStateOf(TextFieldValue("")) }
-    val password = remember { mutableStateOf(TextFieldValue("")) }
+fun RegistrationForm() {
+    val context = LocalContext.current
+    var name by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var phoneNumber by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var address by remember { mutableStateOf("") }
 
-    Column(modifier = modifier.fillMaxWidth().padding(16.dp)) {
-        Text(text = "Username", modifier = Modifier.padding(4.dp).fillMaxWidth())
+    Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+        // Form fields
         TextField(
-            value = username.value,
-            onValueChange = { username.value = it },
-            modifier = Modifier.padding(4.dp).fillMaxWidth()
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
         )
 
-        Text(text = "Password", modifier = Modifier.padding(4.dp).fillMaxWidth())
         TextField(
-            value = password.value,
-            onValueChange = { password.value = it },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.padding(4.dp).fillMaxWidth()
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = phoneNumber,
+            onValueChange = { phoneNumber = it },
+            label = { Text("Nomor Telepon") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            keyboardActions = KeyboardActions(onDone = { /* Handle done */ }),
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        )
 
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            // Tombol Login
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            keyboardActions = KeyboardActions(onDone = { /* Handle done */ }),
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        )
+
+        TextField(
+            value = address,
+            onValueChange = { address = it },
+            label = { Text("Alamat Rumah") },
+            modifier = Modifier.fillMaxWidth().padding(8.dp)
+        )
+
+        // Buttons
+        Row(modifier = Modifier.fillMaxWidth().padding(top = 16.dp)) {
             Button(
                 onClick = {
-                    if (username.value.text == "admin" && password.value.text == "admin") {
-                        Toast.makeText(context, "Login Sukses", Toast.LENGTH_LONG).show()
+                    if (name.isNotEmpty() && username.isNotEmpty() && phoneNumber.isNotEmpty() && email.isNotEmpty() && address.isNotEmpty()) {
+                        Toast.makeText(context, "Halo, $name", Toast.LENGTH_LONG).show()
                     } else {
-                        Toast.makeText(context, "Login Gagal", Toast.LENGTH_LONG).show()
+                        Toast.makeText(context, "Semua inputan harus diisi!", Toast.LENGTH_LONG).show()
                     }
                 },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Purple80,
-                    contentColor = Pink40
-                ),
-                modifier = Modifier.weight(5f).padding(end = 4.dp)
+                modifier = Modifier.weight(1f).padding(4.dp)
             ) {
-                Text(
-                    text = "Login",
-                    style = TextStyle(
-                        color = Color.White,
-                        fontSize = 18.sp
-                    ),
-                    modifier = Modifier.padding(8.dp)
-                )
+                Text("Simpan")
             }
 
-            // Tombol Reset
             Button(
-                modifier = Modifier.weight(5f).padding(start = 4.dp),
                 onClick = {
-                    // Reset username dan password
-                    username.value = TextFieldValue("")
-                    password.value = TextFieldValue("")
+                    name = ""
+                    username = ""
+                    phoneNumber = ""
+                    email = ""
+                    address = ""
                 },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Pink40,
-                    contentColor = Purple80
-                )
+                modifier = Modifier.weight(1f).padding(4.dp)
             ) {
-                Text(
-                    text = "Reset",
-                    style = TextStyle(
-                        color = Color.White,
-                        fontSize = 18.sp
-                    ),
-                    modifier = Modifier.padding(8.dp)
-                )
+                Text("Reset")
             }
         }
     }
@@ -114,8 +109,6 @@ fun FormLogin(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HelloComposeTheme {
-        FormLogin()
-    }
+fun DefaultPreview() {
+    RegistrationForm()
 }
